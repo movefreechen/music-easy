7748a31be52682485c4120c607010345240ddd72
Han Chen <Han.Chen.bb@lingyue-digital.com>
2023-07-04 18:04:13 +0800

    init

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -1,24 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 import * as path from 'path'
 import * as fs from 'fs'
-import Handlebars from 'handlebars'
+import * as Handlebars from 'handlebars'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
-        vscode.commands.registerCommand('catCoding.start', () => {
+        vscode.commands.registerCommand('music.easy.start', () => {
             // Create and show panel
             const panel = vscode.window.createWebviewPanel(
-                'catCoding',
-                'Cat Coding',
+                'musicEasy',
+                'Music Easy',
                 vscode.ViewColumn.One,
-                {}
+                {
+                    enableScripts: true,
+                }
             )
+
+            // And set its HTML content
+            panel.webview.html = getHtmlForWebview(context, panel.webview)
+
+            panel.webview.onDidReceiveMessage(message => {
+                console.log(message)
+            })
         })
     )
+}
+
+function resolve(context: vscode.ExtensionContext, filePath: string) {
+    return path.resolve(context.extensionPath, filePath)
 }
 
 const makeUriAsWebviewUri = (
@@ -36,31 +45,39 @@ const makeUriAsWebviewUri = (
  */
 const getHtmlForWebview = (
     context: vscode.ExtensionContext,
-    webviewView: vscode.Webview,
-    bundleName: string
+    webview: vscode.Webview
 ) => {
-    const htmlTemplateUri = path.resolve(
-        context.extensionPath,
-        './dist/index.html'
-    )
+    const htmlTemplateUri = resolve(context, './dist/index.html')
     const content = fs.readFileSync(htmlTemplateUri, 'utf-8')
+
     const template = Handlebars.compile(content)
 
-	const assets = fs.readdirSync(
-		path.resolve(context.extensionPath, './dist/assets')
-	)
+    const cssUris: string[] = []
+    const scriptUris: string[] = []
+    if (process.env.NODE_ENV === 'production') {
+        const assets = fs.readdirSync(resolve(context, './dist/assets'))
+        for (const asset of assets) {
+            const p = resolve(context, 'dist/assets/' + asset)
+            const uri = makeUriAsWebviewUri(context, webview, p)
 
-	console.log(assets)
+            if (~asset.indexOf('.css')) {
+                cssUris.push(uri)
+            }
 
-    // inject params to template
-    const sidebarBundleWebViewUri = makeUriAsWebviewUri(
-        context,
-        webviewView,
-        `./dist/assets/web-index.js`
-    )
+            if (~asset.indexOf('.js')) {
+                scriptUris.push(uri)
+            }
+        }
+    } else {
+        scriptUris.push(
+            'http://localhost:5173/src/main.ts'
+        )
+    }
 
     const html = template({
-        scriptUris: [sidebarBundleWebViewUri],
+        scriptUris,
+        cssUris,
+        scriptType: process.env.NODE_ENV !=='production' ? 'module' : ''
     })
 
     return html
--- packages/extension/webpack.config.js
+++ packages/extension/webpack.config.js
@@ -1,80 +1,58 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 //@ts-check
-'use strict';
+
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 /** @type WebpackConfig */
-const webExtensionConfig = {
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-	target: 'webworker', // extensions run in a webworker context
-	entry: {
-		'extension': './src/extension.ts'
-	},
-	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, './dist'),
-		libraryTarget: 'commonjs',
-		devtoolModuleFilenameTemplate: '../../[resource-path]',
-		clean: {
-			keep: /assets/
-		}
-	},
-	resolve: {
-		mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
-		extensions: ['.ts', '.js'], // support ts-files and js-files
-		alias: {
-			'handlebars': 'handlebars/dist/handlebars.js'
-			// provides alternate implementation for node module and source files
-		},
-		fallback: {
-			// Webpack 5 no longer polyfills Node.js core modules automatically.
-			// see https://webpack.js.org/configuration/resolve/#resolvefallback
-			// for the list of Node.js core module polyfills.
-			'assert': require.resolve('assert'),
-			'path': require.resolve('path-browserify')
-		}
-	},
-	module: {
-		rules: [{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [{
-				loader: 'ts-loader'
-			}]
-		}]
-	},
-	plugins: [
-		new webpack.optimize.LimitChunkCountPlugin({
-			maxChunks: 1 // disable chunks by default since web extensions must be a single bundle
-		}),
-		new webpack.ProvidePlugin({
-			process: 'process/browser', // provide a shim for the global `process` variable
-		}),
-		new HtmlWebpackPlugin({
-			template: 'index.html'
-		})
-	],
-	externals: {
-		'vscode': 'commonjs vscode', // ignored because it doesn't exist
-		'fs': require('fs')
-	},
-	performance: {
-		hints: false
-	},
-	devtool: 'nosources-source-map', // create a source map that points to the original source file
-	infrastructureLogging: {
-		level: "log", // enables logging required for problem matchers
-	},
-};
+const extensionConfig = {
+    target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-module.exports = [ webExtensionConfig ];
+    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2',
+    },
+    externals: {
+        vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+        // modules added here also need to be added in the .vscodeignore file
+    },
+    resolve: {
+        alias: {
+            handlebars: 'handlebars/dist/handlebars.js',
+        },
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    devtool: 'nosources-source-map',
+    infrastructureLogging: {
+        level: 'log', // enables logging required for problem matchers
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: false,
+        }),
+    ],
+}
+module.exports = [extensionConfig]
